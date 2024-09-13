1ee5feeb9908f3f8b2482e31c22ab20292ee4ee5
FlakoBIG <168838033+FlakoBIG@users.noreply.github.com>
2024-09-13 12:06:34 -0300

    Commit Final
    se cambiaron unas pequeñas cosas pero ahora quedo buenoooo

--- eva1/juegos/views.py
+++ eva1/juegos/views.py
@@ -3,25 +3,25 @@ def inicio(request):
     data={"foto1":"gta.jpg",
           "foto2":"red.jpg",
           "foto3":"bully.jpg",}
-    return render(request,'inicio.html',data)
+    return render(request,'juegos/inicio.html',data)
 
 def gta(request):
     data={"juego":"gta",
           "Descripcion" : "Juego del año",
           "Valoracion":"10/10",
           "foto":"gta.jpg"}
-    return render(request,'juego.html',data)
+    return render(request,'juegos/juego.html',data)
 
 def red(request):
     data={"juego":"red dead redeption 2",
           "Descripcion" : "super bueno",
           "Valoracion":"10000/10",
           "foto":"red.jpg"}
-    return render(request,'juego.html',data)
+    return render(request,'juegos/juego.html',data)
 
 def bully(request):
     data={"juego":"bully",
           "Descripcion" : "muy bueno",
           "Valoracion":"9/10",
           "foto":"bully.jpg"}
-    return render(request,'juego.html',data)
+    return render(request,'juegos/juego.html',data)
